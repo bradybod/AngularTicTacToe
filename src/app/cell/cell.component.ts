1f30c88e9d46410c667c5adfb20c26b5b28177d5
Brady Bodily <bradybod@gmail.com>
2021-04-16 06:21:53 -0600

    Added a new color pallet

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -3,7 +3,10 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-cell',
   template: `
-    <button mat-raised-button color="primary" disableRipple>{{value}}</button>
+    <button *ngIf="!value" mat-raised-button color="primary" disableRipple>{{value}}</button>
+    <button *ngIf="value == 'X'" mat-raised-button color="accent" disableRipple>{{value}}</button>
+    <button *ngIf="value == 'O'" mat-raised-button color="warn" disableRipple>{{value}}</button>
+
   `,
   styleUrls: ['./cell.component.css']
 })
