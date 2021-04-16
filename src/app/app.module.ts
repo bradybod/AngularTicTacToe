80f093ebcd2852a3046a3d96e88221450c901952
Brady Bodily <bradybod@gmail.com>
2021-04-15 18:10:35 -0600

    moved cell and gameboard to app folder and added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CellComponent} from './cell/cell.component';
+import { GameBoardComponent} from './game-board/game-board.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellComponent,
+    GameBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cell/cell.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  template: `
+    <p>
+      cell works!
+    </p>>
+  `,
+  styleUrls: ['./cell.component.css']
+})
+export class CellComponent {
+
+}
